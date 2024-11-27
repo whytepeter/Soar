97cf1d2a0e402c5281f3b0166b9f546b7748dc38
Whyte Peter <peteremmanuelwhyte@gmail.com>
2024-11-27 23:59:15 +0100

    App Router Setup Done

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,13 @@ import AppLayout from "./components/layout/AppLayout";
 
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
-
+import Transactions from "./pages/Transactions";
+import Accounts from "./pages/Accounts";
+import Investments from "./pages/Investments";
+import CreditCards from "./pages/CreditCards";
+import Loans from "./pages/Loans";
+import Services from "./pages/Services";
+import Privileges from "./pages/Privileges";
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
@@ -26,6 +32,13 @@ function App() {
       <AppLayout>
         <Routes>
           <Route path={ROUTES.Dashboard} element={<Dashboard />} />
+          <Route path={ROUTES.Transaction} element={<Transactions />} />
+          <Route path={ROUTES.Accounts} element={<Accounts />} />
+          <Route path={ROUTES.Investments} element={<Investments />} />
+          <Route path={ROUTES.CreditCard} element={<CreditCards />} />
+          <Route path={ROUTES.Loans} element={<Loans />} />
+          <Route path={ROUTES.Services} element={<Services />} />
+          <Route path={ROUTES.Privileges} element={<Privileges />} />
           <Route path={`${ROUTES.Settings}/:tab`} element={<Settings />} />
 
           {/* Redirect for unknown routes */}
--- src/components/layout/sidebar.ts
+++ src/components/layout/sidebar.ts
@@ -1,11 +1,32 @@
 import { MenuItemsType } from "../base/MenuItem";
+import { ROUTES } from "@/types/route";
 
 import homeIcon from "@/assets/icon/home.svg";
 import homeActiveIcon from "@/assets/icon/home-active.svg";
 
+import transactionIcon from "@/assets/icon/transaction.svg";
+import transactionActiveIcon from "@/assets/icon/transaction-active.svg";
+
+import accountIcon from "@/assets/icon/account.svg";
+import accountActiveIcon from "@/assets/icon/account-active.svg";
+
+import investmentIcon from "@/assets/icon/investment.svg";
+import investmentActiveIcon from "@/assets/icon/investment-active.svg";
+
+import cardsIcon from "@/assets/icon/cards.svg";
+import cardsActiveIcon from "@/assets/icon/cards-active.svg";
+
+import privilegeIcon from "@/assets/icon/privilege.svg";
+import privilegeActiveIcon from "@/assets/icon/privilege-active.svg";
+
+import loanIcon from "@/assets/icon/loan.svg";
+import loanActiveIcon from "@/assets/icon/loan-active.svg";
+
+import serviceIcon from "@/assets/icon/service.svg";
+import serviceActiveIcon from "@/assets/icon/service-active.svg";
+
 import settingsIcon from "@/assets/icon/settings.svg";
 import settingsActiveIcon from "@/assets/icon/settings-active.svg";
-import { ROUTES } from "@/types/route";
 
 export const SIDEBAR_ITEMS: MenuItemsType[] = [
   {
@@ -14,6 +35,55 @@ export const SIDEBAR_ITEMS: MenuItemsType[] = [
     icon: homeIcon,
     active: homeActiveIcon,
     to: ROUTES.Dashboard,
+  },
+  {
+    name: "Transactions",
+    slug: "transactions",
+    icon: transactionIcon,
+    active: transactionActiveIcon,
+    to: ROUTES.Transaction,
+  },
+  {
+    name: "Account",
+    slug: "account",
+    icon: accountIcon,
+    active: accountActiveIcon,
+    to: ROUTES.Accounts,
+  },
+  {
+    name: "Investments",
+    slug: "investments",
+    icon: investmentIcon,
+    active: investmentActiveIcon,
+    to: ROUTES.Investments,
+  },
+  {
+    name: "Credit Card",
+    slug: "credit",
+    icon: cardsIcon,
+    active: cardsActiveIcon,
+    to: ROUTES.CreditCard,
+  },
+  {
+    name: "Loans",
+    slug: "loans",
+    icon: loanIcon,
+    active: loanActiveIcon,
+    to: ROUTES.Loans,
+  },
+  {
+    name: "Services",
+    slug: "services",
+    icon: serviceIcon,
+    active: serviceActiveIcon,
+    to: ROUTES.Services,
+  },
+  {
+    name: "My Privileges",
+    slug: "privilege",
+    icon: privilegeIcon,
+    active: privilegeActiveIcon,
+    to: ROUTES.Privileges,
   },
   {
     name: "Settings",
