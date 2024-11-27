85019f02f762a0ae35ab53d1c2740a023981011d
Whyte Peter <peteremmanuelwhyte@gmail.com>
2024-11-27 20:43:44 +0100

    Added Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import { Toaster } from "react-hot-toast";
 
 import Loading from "./components/ui/loading";
 import AppLayout from "./components/layout/AppLayout";
 
-import Dashboard from "./pages/Dashboard";
 import { ROUTES } from "./types/route";
+import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: 14,
+          },
+        }}
+      />
+
       <AppLayout>
         <Routes>
           <Route path={ROUTES.Dashboard} element={<Dashboard />} />
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,83 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import moment from "moment";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatNumber = (num: number) => {
+  if (isNaN(num)) return num;
+  return +num.toLocaleString();
+};
+
+export const formatCurrency = (amount: number) => {
+  if (isNaN(amount) || amount === 0) {
+    return "₦0.00";
+  }
+  return amount.toLocaleString("en-NG", {
+    style: "currency",
+    currency: "NGN",
+  });
+};
+
+export const formatDate = (
+  arg: Date | string | any,
+  style = "Do MMM, YYYY",
+  pastTime = false
+): string => {
+  if (!arg) return "N/A";
+  let date = arg as Date;
+
+  if (!pastTime) return moment(date).format(style);
+
+  return moment(date).fromNow();
+};
+
+export const validateEmail = (email: string) => {
+  if (email) {
+    return email.match(
+      /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    );
+  }
+};
+
+export const moveCenter = <T>(
+  e: MouseEvent | React.MouseEvent<T, MouseEvent> | null,
+  el?: T
+): void => {
+  const element = (e?.target as HTMLElement) || el;
+  if (!element) return;
+
+  if ("scrollIntoView" in element) {
+    (element as HTMLElement).scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
+  }
+};
+
+export const copyText = async (text: string, message: string = "Copied") => {
+  await navigator.clipboard.writeText(text);
+  // toast(message);
+};
+
+export const initials = (name: string): string => {
+  if (!name) return "N/A";
+
+  // Split the name into parts
+  const parts = name.trim().split(/\s+/);
+
+  // Get the first letter of the first and second parts
+  const firstInitial = parts[0]?.charAt(0).toUpperCase() || "";
+  const secondInitial = parts[1]?.charAt(0).toUpperCase() || "";
+
+  return `${firstInitial}${secondInitial}`;
+};
+
+export const generateUniqueId = (length: number = 10): string => {
+  return Math.random()
+    .toString(36)
+    .substring(2, length + 2);
+};
