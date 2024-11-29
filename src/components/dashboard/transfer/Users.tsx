01c131d2ac44e0f3e1982ed36455e3d3085bb79b
Whyte Peter <peteremmanuelwhyte@gmail.com>
2024-11-30 00:52:22 +0100

    Add Carousel

--- src/components/dashboard/transfer/Users.tsx
+++ src/components/dashboard/transfer/Users.tsx
@@ -1,30 +1,39 @@
 import UserAvatar from "@/assets/img/avatar.png";
 import Avatar from "@/components/base/Avatar";
 
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { useSwiper } from "swiper/react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Users() {
-  const swiper = useSwiper();
   return (
     <div className="border">
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        slidesPerView={3}
-      >
-        {[1, 2, 3, 4, 5, 6]?.map((index) => (
-          <SwiperSlide key={index}>
-            <div className="flex flex-col  items-center w-fit gap-1">
-              <Avatar className="w-16 h-16" img={UserAvatar} />
-              <div className="flex flex-col items-center w-fit">
-                <h4 className="text-dark text-sm md:text-base">Livia Bator</h4>
-                <span className="text-dark-200 text-xs sm:text-sm">CEO</span>
+      <Carousel>
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem
+              key={index}
+              className="pl-1 md:basis-1/2 lg:basis-1/3"
+            >
+              <div className="flex flex-col  items-center w-fit gap-1">
+                <Avatar className="w-16 h-16" img={UserAvatar} />
+                <div className="flex flex-col items-center w-fit">
+                  <h4 className="text-dark text-sm md:text-base">
+                    Livia Bator
+                  </h4>
+                  <span className="text-dark-200 text-xs sm:text-sm">CEO</span>
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselNext />
+        <CarouselPrevious />
+      </Carousel>
     </div>
   );
 }
